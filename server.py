aa17e78eaa5cf83027e4bd2d8acc02207e0434c5
remulus123 <murungiremulus73@gmail.com>
2023-04-26 13:10:07 +0300

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 print(__name__)
 
 
-@app.route('/index.html')  # index.html root page
+@app.route('/')  # index.html root page
 def my_home():
     return render_template('index.html')
 
@@ -17,31 +17,36 @@ def html_page(page_name):
 
 def write_to_file(data):  # writing to the database/text(database.txt) file so that information can be viewed later
     with open('database.txt', mode='a') as database:
+        name  = data["name"]
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
-        file = database.write(f'\n {email},{subject},{message}')
+        file = database.write(f'\n {name},{email},{subject},{message}')
 
 
 def write_to_csv(data):  # writing to the database/text(database.csv) file so that information can be viewed later
     with open('database.csv', newline='', mode='a') as database2:
+        name  = data["name"]
         email = data["email"]
         subject = data["subject"]
         message = data["message"]
         csv_writer = csv.writer(database2, delimiter=',', quotechar=';',
                                 quoting=csv.QUOTE_MINIMAL)  # for more info check csv python documentation
-        csv_writer.writerow([email, subject, message])
+        csv_writer.writerow([name,email, subject, message])
 
 
 @app.route('/submit_form', methods=['POST', 'GET'])  # submitting the form by the user and redirecting the user to
 # thankyou.html page
+
 def submit_form():
     if request.method == 'POST':
         try:
             data = request.form.to_dict()
             write_to_csv(data)
-            return redirect('/thankyou.html')
+
         except:
             return 'did not save to the database'
     else:
         return 'something went wrong.Try again'
+    name = request.form['name']
+    return render_template('thankyou.html', name=name)
